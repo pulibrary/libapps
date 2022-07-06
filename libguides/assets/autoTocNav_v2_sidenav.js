e7c15b7c8c4e84635d0ba4a788984005c36d5100
Jane Sandberg <sandbergja@users.noreply.github.com>
2022-07-06 10:11:30 -0700

    Don't create two <a> tags for a single link (#14)
    Co-authored-by: Max Kadel <maxkadel@users.noreply.github.com>
    Co-authored-by: Vickie Karasic <vickiekarasic@users.noreply.github.com>

--- libguides/assets/autoTocNav_v2_sidenav.js
+++ libguides/assets/autoTocNav_v2_sidenav.js
@@ -3,25 +3,29 @@
 // This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License, 
 // http://creativecommons.org/licenses/by-nc-sa/4.0/
 
-// I need to fix the window scrolling issue with long boxes.
-
-//you need to first put all your boxes in the center column and create an empty box in the left column for the TOC
+// Put all of your content boxes in the second column and create an empty box in the left column for the TOC
+// If using on just one page, then in the TOC box add a Media/Widget content item and enter the links for the script and CSS
 $(document).ready(function() {
 	var $boxIDs = [];
-//get box IDs
-	$('div#s-lg-col-1 div[id^="s-lg-box-"]').each(function() {	
-		var patt1 = /s-lg-box-\d+$/;				
-		if (patt1.exec($(this).attr("id"))) {
+	var $boxTitle = [];
+// get box titles
+	$('div[id="s-lg-col-2"] .s-lib-box-title').each(function() {		
+		$boxTitle.push($(this).text());
+	});
+// get box IDs for second column only		
+	$('div[id="s-lg-col-2"] div.s-lib-box').each(function() {		
+		var patt2 = /s-lg-box-\d+$/;
+		// $(this).prepend("<p>Found this box</p>"); for testing
+		if (patt2.test($(this).attr("id"))) {
 			$boxIDs.push($(this).attr("id"));
 		} 
-	});	
-// fix the scrolling problem by adding an <a> tag with the box title text that doesn't use the anchor bookmark
-	$('ul.s-lg-boxnav a').each(function() {
-		var aText = $(this).text();
-		$(this).empty();				
-		$("<a>"+aText+"</a>").insertAfter($(this));				
 	});
-		
+//populate the nav box with box titles
+	var $tocBox = $('div[id="s-lg-col-1"] div[id^="s-lg-box-collapse-"]');
+	$tocBox.prepend('<ul class="list-group s-lg-boxnav"></ul>');
+	for (i=0; i<$boxTitle.length; i++) {
+		$('ul.s-lg-boxnav').append('<li class="list-group-item boxNav"><a href="#'+$boxIDs[i]+'">'+$boxTitle[i]+'</a>');
+	}
 // if url is for an indiv box, select that one and hide other content boxes; if not, show only first content box 
 // and make the first TOC entry the currently selected item
 	if (location.hash) {
@@ -42,8 +46,7 @@ $(document).ready(function() {
 			$('div[id='+$boxIDs[i]+']').hide();					
 		}
 		$('li.list-group-item').filter(':first').addClass('currentNav').prepend('<i class="fa fa-angle-double-right fa-lg fa-fw"></i>');
-		}
-	$('li.list-group-item').addClass('boxNav');
+		}	
 //show the selected box when clicked
 	$('.boxNav').click(function() {
     	var selectedNav = $('.currentNav');
@@ -51,14 +54,13 @@ $(document).ready(function() {
 	    $(this).prepend($('.fa-angle-double-right')); // move the font awesome <i> tag to the item clicked on
 	    $(this).toggleClass('currentNav');	    
 	    for (i=0; i<$boxIDs.length; i++) {
-			if ($(this).find("a").attr("href").slice(1) == $boxIDs[i]) { // need to slice this because the href begins with #				
+			if ($(this).find("a").attr("href").slice(1) == $boxIDs[i]) { // need to slice this because the href begins with #
 				$('div[id='+$boxIDs[i]+']').show();
 				window.location.hash = $boxIDs[i];
-				// $('html').scrollTop(0);
-				// $('div[id='+$boxIDs[i]+']').prepend($vertLoc);	
+				window.scrollTo(0,0);
 			} else {
 				$('div[id='+$boxIDs[i]+']').hide();				
 			}
-		};			
+		}
 	});
 });
